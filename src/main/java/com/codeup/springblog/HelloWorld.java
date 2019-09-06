2a06786e12a1748fc3fae347f8983ecfab5e1c94
Lam Nguyen <nguyen.h.lam12@gmail.com>
2019-09-06 10:13:11 -0500

    controllers

---
+++ src/main/java/com/codeup/springblog/HelloWorld.java
@@ -0,0 +1,15 @@
+package com.codeup.springblog;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HelloWorld {
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String helloWorld() {
+        return "Hello World from the Controller";
+    }
+}
